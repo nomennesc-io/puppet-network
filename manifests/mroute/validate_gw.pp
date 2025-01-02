91e32438710319bc6392122c99142f01cf40aa7a
Nomen Nescio <info@nomennesc.io>
2025-01-02 18:55:47 +0100

    remove deprecated validate_ functions

--- manifests/mroute/validate_gw.pp
+++ manifests/mroute/validate_gw.pp
@@ -1,6 +1,5 @@
 # GWs validator. We need it to have compatibility with puppet 3.x, 'cause it doesn't support each.
 #
 define network::mroute::validate_gw ($routes) {
-  $route = $routes[$name]
-  validate_string($route)
+  String $route = $routes[$name]
 }
